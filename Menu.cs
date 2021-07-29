97988e2caa6e4678f4712995624f22e16b137556
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-28 20:12:26 -0500

    Add project files.

---
+++ Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ch4_studio
+{
+    class Menu
+    {
+        public DateTime lastUpdated { get; set; }
+        public List<MenuItem> Items { get; set; }
+        public Menu(List<MenuItem> items)
+        {
+            Items = items;
+            lastUpdated = DateTime.Now;
+        }
+
+        public bool addItemToMenu(MenuItem newItem)
+        {
+            if (!Items.Contains(newItem))
+            {
+                Items.Add(newItem);
+                lastUpdated = DateTime.Now;
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch4_studio
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool isNew { get; set; } = true;
+
+        public MenuItem(string name)
+        {
+            Name = name;
+        }
+
+        public MenuItem(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public MenuItem(string name, double price, string description)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+
+        public MenuItem(string name, double price, string description, string category)
+        {
+            Name = name;
+            Price = price;
+            Description = description;
+            Category = category;
+        }
+    }
+}
