70a600dcb76421811c43a1c722b11b4ecd3f8a79
Megan Daubenmier <megan.daubenmier@gmail.com>
2021-07-28 20:11:21 -0500

    Studio completed.

---
+++ ch4-studio/ch4-studio/ch4-studio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch4_studio
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<MenuItem> myItems = new List<MenuItem>();
+            MenuItem item1 = new MenuItem("Mashed potatoes");
+            MenuItem item2 = new MenuItem("Green eggs and ham");
+            MenuItem item3 = new MenuItem("Spaghetti");
+            myItems.Add(item1);
+            myItems.Add(item2);
+            Menu myMenu = new Menu(myItems);
+            Console.WriteLine(myMenu.lastUpdated);
+            Console.WriteLine(myMenu.addItemToMenu(item3));
+            Console.WriteLine(myMenu.lastUpdated);
+        }
+    }
+}
